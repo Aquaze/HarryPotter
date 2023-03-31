0220b3a9b31f75b47ea9c4e387ebbef8c2d90926
Aquaze <88437016+Aquaze@users.noreply.github.com>
2023-03-31 12:32:20 +0200

    Préparation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,27 +6,16 @@ public class Main {
     public static void main(String[] args) {
 
 
-
-        System.out.println("Choisi un nom");
+        System.out.println("Hello wizard, welcome to hogwarts. Ready to discover the mysteries of magic? What is your name?");
+        System.out.println("Choose a name");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        /**
-        System.out.println("Choisi une classe : 1 - Gryffondor, 2 - Serpentard, 3 - Poufsouffle, 4 - Serdaigle");
-        int houseID = InputHandler.getValidInput(1,4)-1;
+        System.out.println("Your ID");
 
-        System.out.println("Choisi un core pour ta baguette : 1 -Pheonix, 2 - Dragon, 3 - Corne de licorne, 4 - Bois de cerf");
-        int core = InputHandler.getValidInput(1,4);
-        System.out.println("Choisi une longueur pour ta baguette : 5cm - 30cm");
-        int length = InputHandler.getValidInput(5,30);
-
-        System.out.println("Choisi un Animal de compagnie : 1 - Owl , 2 - Rat, 3 - Cat, 4 - Toad");
-        Pet pet = Pet.values()[InputHandler.getValidInput(1,4)-1];
-        Wand wand = new Wand(core, length);
-        House house = new House(houseID);
-         **/
-
-
-        Wizard player = new Wizard(name, Pet.OWL, new Wand(1, 10), new House(0));
+        Pet[] possiblePets = {Pet.CAT, Pet.RAT, Pet.TOAD, Pet.OWL};
+        int randomIndex = (int) (Math.random() * possiblePets.length);
+        Pet randomPet = possiblePets[randomIndex];
+        Wizard player = new Wizard(name, randomPet , new Wand(1, 10), new House(0));
         player.setProbOfProtego(0);
 
         player.addSpell(new Stuppefix());
